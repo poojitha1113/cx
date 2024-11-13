e39ee4deb31c4550d5113ba62cec912d15f50f22
poojitha1113 <poojitha.gutha1113@gmail.com>
2024-11-13 14:08:54 -0500

    code1

---
+++ Revers.java
@@ -0,0 +1,14 @@
+class Revers{
+public static void main(String[]args)
+{
+String name="poojitha";
+System.out.println(name);
+System.out.println(name.length());
+
+for(int i=name.length();i>0;i--)
+	{
+		System.out.print(name.charAt(i));
+	}
+
+}
+}
